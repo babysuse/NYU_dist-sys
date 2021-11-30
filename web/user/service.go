2d8ce4a6af11e51318ac57e9f030fc4b4f30ac86
babysuse <babysuse2018@gmail.com>
2021-11-30 04:40:23 -0500

    Add gRPC server for Follow/Unfollow

---
+++ web/user/service.go
@@ -0,0 +1,82 @@
+package user
+
+import (
+	"context"
+	"log"
+	"net"
+
+	database "github.com/os3224/final-project-0b5a2e16-babysuse/internal/pkg/db/migrations/mysql"
+	"github.com/os3224/final-project-0b5a2e16-babysuse/web/user/pb"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":16028"
+)
+
+type Server struct {
+	pb.UnimplementedUserServiceServer
+}
+
+func (srv *Server) Follow(ctx context.Context, req *pb.FollowRequest) (*pb.FollowReply, error) {
+	stmt, err := database.DB.Prepare("INSERT INTO Following(Username, Followeename) VALUES(?, ?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = stmt.Exec(req.Username, req.Followeename)
+	if err != nil {
+		log.Println(err)
+		return &pb.FollowReply{}, err
+	}
+	return &pb.FollowReply{Result: "Followed"}, nil
+}
+
+func (srv *Server) Unfollow(ctx context.Context, req *pb.FollowRequest) (*pb.FollowReply, error) {
+	stmt, err := database.DB.Prepare("DELETE FROM Following WHERE Username = ? AND Username = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = stmt.Exec(req.Username, req.Followeename)
+	if err != nil {
+		log.Println(err)
+		return &pb.FollowReply{}, err
+	}
+	return &pb.FollowReply{Result: "Unfollowed"}, nil
+}
+
+func (srv *Server) GetFollowee(ctx context.Context, req *pb.GetFolloweeRequest) (*pb.GetFolloweeResponse, error) {
+	rows, err := database.DB.Query(`
+		SELECT Followeename
+		FROM Following
+		WHERE Username = ?`, req.Username)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var response pb.GetFolloweeResponse
+	for rows.Next() {
+		var followee pb.User
+		err := rows.Scan(&followee.Username)
+		if err != nil {
+			log.Fatal(err)
+		}
+		response.Followees = append(response.Followees, &followee)
+	}
+	return &response, nil
+}
+
+func NewUserServiceServer() {
+	go func() {
+		bind, err := net.Listen("tcp", port)
+		if err != nil {
+			log.Fatalf("failed to bind: %v", err)
+		}
+		srv := grpc.NewServer()
+		pb.RegisterUserServiceServer(srv, &Server{})
+		log.Printf("user service listening at %v", bind.Addr())
+		if err := srv.Serve(bind); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	}()
+}
