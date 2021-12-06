e9459abd69a7637d8baeb402a96d11142348b255
babysuse <babysuse2018@gmail.com>
2021-12-06 10:03:22 -0500

    Rename & merge login.go & signup.go

--- web/account.go
+++ web/account.go
@@ -57,3 +57,38 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	})
 	log.Printf("%s logged in", cred.Username)
 }
+
+func Signup(w http.ResponseWriter, r *http.Request) {
+	// decode request
+	var cred Credentials
+	err := json.NewDecoder(r.Body).Decode(&cred)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	log.Printf("%s logging in", cred.Username)
+
+	// set up RPC client
+	conn, err := grpc.Dial(AccountSrvAddr, grpc.WithInsecure(), grpc.WithTimeout(time.Second))
+	if err != nil {
+		log.Fatalf("failed to connect: %v", err)
+	}
+	defer conn.Close()
+	client := accountpb.NewAccountServiceClient(conn)
+
+	// contact RPC server
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+	resp, err := client.Signup(ctx, &accountpb.Account{Username: cred.Username, Password: cred.Password})
+	if err != nil {
+		log.Fatalf("failed to signup: %v", err)
+	}
+
+	// set cookie with expiry time of a day
+	http.SetCookie(w, &http.Cookie{
+		Name:    "session_token",
+		Value:   resp.Token,
+		Expires: time.Now().Add(24 * time.Hour),
+	})
+	log.Printf("%s signed up", cred.Username)
+}
